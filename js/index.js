08549f7cd0852c38f0340aafb43e56ff3be84839
yash desai <yhdesai99@ymail.com>
2019-08-24 15:37:26 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
 var h1 = document.createElement("h1");
+h1.innerHTML = "Hello World"
 var body = document.querySelector("body")
 body.appendChild(h1);
